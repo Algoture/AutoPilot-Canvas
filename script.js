e9368f680649460a511ef81ed14c80cba11ad2b9
Umesh Nagare <umeshnagare27@gmail.com>
2024-07-27 23:12:56 +0530

    Scratch start

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let canvas = document.getElementById("myCanvas");
 canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 let ctx = canvas.getContext("2d");
-ctx.rect(10,10,100,100);
-ctx.fillStyle()
-
+car = new Car(100, 100, 50, 50);
+car.draw(ctx);
+car2 = new Car(200, 200, 50, 50);
+car2.draw(ctx);
