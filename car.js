6c237da23d9db538a14bd645bbc85c28c0343dfb
Umesh Nagare <umeshnagare27@gmail.com>
2024-07-30 14:02:18 +0530

    Controls

--- car.js
+++ car.js
@@ -2,11 +2,52 @@ class Car {
   constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
+    this.speed = 0;
+    this.acceleration = 0.9;
     this.width = width;
+    this.friction = 0.05;
+    this.maxSpeed = 4;
     this.height = height;
+    this.controls = new Controls();
+  }
+  update() {
+    // controls:
+    if (this.controls.up) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.down) {
+      this.speed -= this.acceleration;
+    }
+    if (this.controls.left) {
+      this.x -= 2;
+    }
+    if (this.controls.right) {
+      this.x += 2;
+    }
+    // speed, acc, friction:
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    this.y -= this.speed;
   }
   draw(ctx) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    // ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.rect(
+      this.x - this.width / 2,
+      this.y - this.height / 2,
+      this.width,
+      this.height
+    );
+    ctx.fill();
   }
 }
--- script.js
+++
@@ -1,8 +0,0 @@
-let canvas = document.getElementById("myCanvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-let ctx = canvas.getContext("2d");
-car = new Car(100, 100, 50, 50);
-car.draw(ctx);
-car2 = new Car(200, 200, 50, 50);
-car2.draw(ctx);
